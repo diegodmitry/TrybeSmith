dea41081ef56e1602e939a3dcf71f4309420a6e7
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-09 19:38:15 -0300

    feat: added create method into products.service.ts file.

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,6 +1,6 @@
 import connection from '../models/connection';
 import ProductModel from '../models/products.model';
-import { IProduct } from '../interfaces/products.interface';
+import { IProduct, IProductCreate } from '../interfaces/products.interface';
 
 export default class ProductService {
   public model: ProductModel;
@@ -12,5 +12,9 @@ export default class ProductService {
   public async getAll(): Promise<IProduct[]> {
     const products = await this.model.getAll();
     return products;
+  }
+
+  public create(product: IProductCreate): Promise<IProduct> {
+    return this.model.create(product);
   }
 }
