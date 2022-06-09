d6dc198966b4df843b27ec9f210b0280fc0eac5a
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-09 15:20:39 -0300

    feat: added products.model.ts file.

---
+++ src/models/products.model.ts
@@ -0,0 +1,20 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { IProduct } from '../interfaces/products.interface';
+
+
+export default class ProductModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<IProduct[]> {
+    const query = 'SELECT * FROM `Trybesmith.Products`';
+    const result = await this.connection.execute(query);
+    const [products] = result;
+    return products as IProduct[];
+  }
+}
+
+
