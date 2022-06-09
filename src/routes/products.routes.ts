92004bd8f28e5db9546e095827b5e8522f323d68
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-09 19:42:59 -0300

    feat: added validationFieldProduct middleware, added productsController.create into products.route.ts file.

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller';
+import validateFieldProduct from '../middlewares/product.validation';
 
 const router = Router();
 
 const productsController = new ProductsController();
 
 router.get('/products', productsController.getAll);
+
+router.post('/products', validateFieldProduct, productsController.create);
 
 export default router;
