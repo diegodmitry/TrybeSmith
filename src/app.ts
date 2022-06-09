7c6ad179d53d82e5c8e2ae90aad139527ea60195
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-09 19:40:55 -0300

    feat: added errorHandler router into app.ts file.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 
 import ProductsRouter from './routes/products.routes';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(ProductsRouter);
+
+app.use(errorHandler);
 
 export default app;
