1f718668434faeed74cfdb5df09faabc734d1fd1
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-09 19:39:18 -0300

    feat: added create method into products.controller.ts file.

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -8,6 +8,13 @@ class ProductsController {
     const prodcuts = await this.productService.getAll();
     return res.status(200).json(prodcuts);
   };
+
+  public create = async (req: Request, res: Response) => {
+    const product = req.body;
+
+    const productCreated = await this.productService.create(product);
+    res.status(201).json(productCreated);
+  };
 }
 
 export default ProductsController;
