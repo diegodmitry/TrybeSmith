434b96884602d503a571202b478ee21566ce21f3
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-09 17:30:16 -0300

    feat: added ProductsRouter router.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import ProductsRouter from './routes/products.routes';
+
 const app = express();
 
 app.use(express.json());
+
+app.use(ProductsRouter);
 
 export default app;
