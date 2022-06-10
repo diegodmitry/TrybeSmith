56b8c92e2f50146d397134430b7d1e93770e76bc
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-10 16:16:57 -0300

    feat; added login.controller.ts file.

---
+++ src/controllers/login.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import LoginService from '../services/login.service';
+
+class LoginController {
+  constructor(private loginService = new LoginService()) { }
+
+  public login = async (req: Request, res: Response) => {
+    const user = req.body;
+    const token = await this.loginService.login(user);
+    if (!token) return res.status(401).json({ message: 'Username or password invalid' });
+    return res.status(200).json({ token });
+  };
+}
+
+export default LoginController;
