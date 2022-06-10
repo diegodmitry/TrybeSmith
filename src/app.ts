22a07f295ff21f71f5586efb8879f61179ec4a44
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-10 13:56:22 -0300

    feat: added OrdersRouter router.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import ProductsRouter from './routes/products.routes';
 import UsersRouter from './routes/users.routes';
+import OrdersRouter from './routes/orders.routes';
 import errorHandler from './middlewares/errorHandler';
 
 const app = express();
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(ProductsRouter);
 
 app.use(UsersRouter);
+
+app.use(OrdersRouter);
 
 app.use(errorHandler);
 
