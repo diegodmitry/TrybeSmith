74939ff8674ffe2ca5c2648eb798590b0540a201
Diego Dmitry <diegodmitry@hotmail.com>
2022-06-10 09:55:49 -0300

    feat: added UsersRouter router into app.ts file.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import ProductsRouter from './routes/products.routes';
+import UsersRouter from './routes/users.routes';
 import errorHandler from './middlewares/errorHandler';
 
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.json());
 
 app.use(ProductsRouter);
+
+app.use(UsersRouter);
 
 app.use(errorHandler);
 
